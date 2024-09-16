59b274a8fb8f7bc15c7e5fa3bdf5abf15350860c
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-16 23:42:43 +0000

    Refactor delete user form and authentication routes

--- VerbMeister-app/routes/auth.php
+++ VerbMeister-app/routes/auth.php
@@ -56,4 +56,6 @@ Route::middleware('auth')->group(function () {
 
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->name('logout');
+    Route::post('delete-account', [PasswordController::class, 'profile.destroy'])
+                ->name('delete-account');
 });
