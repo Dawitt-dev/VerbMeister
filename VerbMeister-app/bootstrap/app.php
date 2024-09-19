3e9ee1e0388d1e4e2ad2794ce2526ee4d2e37078
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-19 04:52:37 +0000

    Refactor app.php to add necessary configuration classes

--- VerbMeister-app/bootstrap/app.php
+++ VerbMeister-app/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Foundation\Configuration\Providers;
+use Illuminate\Foundation\Configuration\Routes;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
