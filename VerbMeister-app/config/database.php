c11ea2c997a09e5d20d19084ec3159930145ac2f
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-19 03:41:37 +0000

    Refactor database configuration to use 'forge' as the default value for the host in mysql driver

--- VerbMeister-app/config/database.php
+++ VerbMeister-app/config/database.php
@@ -45,7 +45,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'forge'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
