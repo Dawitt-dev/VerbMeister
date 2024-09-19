2e7f363e5e872bae1364ad009418d5b1002ca33d
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-19 01:04:10 +0000

    Refactor vite.config.js to include server configuration for hosting and hot module replacement

--- VerbMeister-app/vite.config.js
+++ VerbMeister-app/vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            clientPort: process.env.CODESPACE_NAME ? 443 : 5173,
+        },
+    },
     plugins: [
         laravel({
             input: [
