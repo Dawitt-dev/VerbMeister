c8372e882cad5c67978294cabb8b8d4e821d7354
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-19 05:06:16 +0000

    Refactor app.php to add middleware registration

--- VerbMeister-app/bootstrap/app.php
+++ VerbMeister-app/bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->register();
+    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
