1dbc0e4c7b36dc27632cde0f24c3c58fc817743e
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-19 05:09:51 +0000

    Refactor app.php to remove middleware registration

--- VerbMeister-app/bootstrap/app.php
+++ VerbMeister-app/bootstrap/app.php
@@ -13,9 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->register();
-    })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
