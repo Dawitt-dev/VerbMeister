0dfa4279e4b1a7f80b7d683825463d2bf90e517e
Dawitt <126588225+Dawitt-dev@users.noreply.github.com>
2024-09-19 01:05:08 +0000

    Refactor app.js and Quiz.vue components

--- VerbMeister-app/resources/js/app.js
+++ VerbMeister-app/resources/js/app.js
@@ -1,12 +1,2 @@
 import './bootstrap';
-import Quiz from './Components/Quiz.vue';
 
-import Alpine from 'alpinejs';
-
-const app = createApp({});
-app.component('quiz', Quiz);
-app.mount('#app');
-
-window.Alpine = Alpine;
-
-Alpine.start();
